f1a56b6db7bfc20d744ba4eaac4b9f5749c6286f
Paulus Esterhazy <pesterhazy@gmail.com>
2017-11-22 14:55:00 +0100

    Try compiling manually

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ rm -rf out
 rm -rf out.js
 java -cp cljs-1.9.946.jar:src clojure.main compile.clj
 pretty-js < out.js > out.pretty.js
+
+java -cp cljs-1.9.946.jar com.google.javascript.jscomp.CommandLineRunner --compilation_level ADVANCED --js out/'**.js' --manage_closure_dependencies --closure_entry_point bug.core --js_output_file advanced.js --third_party
