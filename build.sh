f0f4a64c28254cfa0b4616ae9e7b00d5a5b5ebf8
Paulus Esterhazy <pesterhazy@gmail.com>
2017-11-22 15:27:29 +0100

    Fix

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ rm -rf out.js
 java -cp cljs-1.9.946.jar:src clojure.main compile.clj
 pretty-js < out.js > out.pretty.js
 
-java -cp cljs-1.9.946.jar com.google.javascript.jscomp.CommandLineRunner --compilation_level ADVANCED --js out/'**.js' --manage_closure_dependencies --closure_entry_point bug.core --js_output_file advanced.js --third_party
+perl -i.bak -pe 's/find_ns_obj_STAR_\(global/find_ns_obj_STAR_\(goog.global/g' out/cljs/core.js
+
+java -cp cljs-1.9.946.jar com.google.javascript.jscomp.CommandLineRunner --compilation_level ADVANCED --js out/'**.js' --manage_closure_dependencies --closure_entry_point bug.core --closure_entry_point process.env --js_output_file advanced.js
+pretty-js < advanced.js > advanced.pretty.js
